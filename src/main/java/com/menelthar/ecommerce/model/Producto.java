b08166197045bcbb41f14ad7c6810d520bfc6b8c
Menelthar <maaxbj@gmail.com>
2023-05-04 18:12:11 -0600

    crear las clases model jpa y vistas

--- src/main/java/com/menelthar/ecommerce/model/Producto.java
+++ src/main/java/com/menelthar/ecommerce/model/Producto.java
@@ -1,7 +1,18 @@
 package com.menelthar.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
@@ -9,10 +20,14 @@ public class Producto {
 	private double precio;
 	private int cantidad;
 
+	@ManyToOne
+	private Usuario usuario;
+
 	public Producto() {
 	}
 
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -20,6 +35,7 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
 
 	public Integer getId() {
@@ -70,10 +86,18 @@ public class Producto {
 		this.cantidad = cantidad;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	@Override
 	public String toString() {
 		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
-				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
+				+ ", precio=" + precio + ", cantidad=" + cantidad + ", usuario=" + usuario + "]";
 	}
 
 }
