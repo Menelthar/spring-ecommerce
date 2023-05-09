5d9e6ca2e1cb275da96be255b1c08e1ea9a27085
Menelthar <maaxbj@gmail.com>
2023-05-09 15:26:47 -0600

    Metodo para generar el número de la orden

--- src/main/java/com/menelthar/ecommerce/service/IOrdenService.java
+++ src/main/java/com/menelthar/ecommerce/service/IOrdenService.java
@@ -1,8 +1,12 @@
 package com.menelthar.ecommerce.service;
 
+import java.util.List;
+
 import com.menelthar.ecommerce.model.Orden;
 
 public interface IOrdenService {
+
+	List<Orden> findAll();
 
 	Orden save(Orden orden);
 
