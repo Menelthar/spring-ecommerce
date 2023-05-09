98e0d90f3cfa323ffdf53294b1a4d5c3ee6e5a66
Menelthar <maaxbj@gmail.com>
2023-05-08 20:26:08 -0600

    se agrega la funcionalidad de ver el carrito todo el tiempo y validar que no se dupliquen los productos

--- src/main/java/com/menelthar/ecommerce/controller/HomeController.java
+++ src/main/java/com/menelthar/ecommerce/controller/HomeController.java
@@ -70,7 +70,13 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 
-		detalles.add(detalleOrden);
+		// validar que el producto no se añada 2 veces
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+		if (!ingresado) {
+			detalles.add(detalleOrden);
+		}
 
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
@@ -101,8 +107,17 @@ public class HomeController {
 
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
-		
+
 		return "usuario/carrito";
+	}
+
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "/usuario/carrito";
 	}
 
 }
