1a3729e6a24bab2e2cdaf1fad66574e6213b816a
Menelthar <maaxbj@gmail.com>
2023-05-08 18:44:05 -0600

    mostrar la vista del producto al usuario

--- src/main/java/com/menelthar/ecommerce/controller/HomeController.java
+++ src/main/java/com/menelthar/ecommerce/controller/HomeController.java
@@ -1,17 +1,21 @@
 package com.menelthar.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.menelthar.ecommerce.repository.ProductoRepository;
 import com.menelthar.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService; 
@@ -22,6 +26,12 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("Id producto enviado como parametro {}", id );
+		return "usuario/productohome";
 	}
 	
 }
