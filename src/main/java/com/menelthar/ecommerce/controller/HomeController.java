75189d01274a9da358e1df80448f4ce7e58a22bc
Menelthar <maaxbj@gmail.com>
2023-05-10 12:52:40 -0600

    implementar template diferente para usuario loggeado

--- src/main/java/com/menelthar/ecommerce/controller/HomeController.java
+++ src/main/java/com/menelthar/ecommerce/controller/HomeController.java
@@ -59,6 +59,9 @@ public class HomeController {
 
 		model.addAttribute("productos", productoService.findAll());
 
+		// session
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+
 		return "usuario/home";
 	}
 
@@ -131,10 +134,13 @@ public class HomeController {
 	}
 
 	@GetMapping("/getCart")
-	public String getCart(Model model) {
+	public String getCart(Model model, HttpSession session) {
 
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
+
+		// Sesion
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
 
 		return "/usuario/carrito";
 	}
