0575d949534989df2301eb23bc69ae078a3c65ba
Menelthar <maaxbj@gmail.com>
2023-05-10 15:43:10 -0600

    Funcionalidad para ver los detalles de las ordenes para el admin

--- src/main/java/com/menelthar/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/menelthar/ecommerce/controller/AdministradorController.java
@@ -2,12 +2,16 @@ package com.menelthar.ecommerce.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.menelthar.ecommerce.model.Orden;
 import com.menelthar.ecommerce.model.Producto;
 import com.menelthar.ecommerce.service.IOrdenService;
 import com.menelthar.ecommerce.service.IProductoService;
@@ -26,6 +30,8 @@ public class AdministradorController {
 	@Autowired
 	private IOrdenService ordenService;
 
+	private Logger logg = LoggerFactory.getLogger(AdministradorController.class);
+
 	@GetMapping("")
 	public String home(Model model) {
 		List<Producto> productos = productoService.findAll();
@@ -43,6 +49,14 @@ public class AdministradorController {
 	public String ordenes(Model model) {
 		model.addAttribute("ordenes", ordenService.findAll());
 		return "administrador/ordenes";
+	}
+
+	@GetMapping("/detalle/{id}")
+	public String detalle(Model model, @PathVariable Integer id) {
+		logg.info("Id de la orden: {}", id);
+		Orden orden = ordenService.findById(id).get();
+		model.addAttribute("detalles", orden.getDetalle());
+		return "administrador/detalleorden";
 	}
 
 }
